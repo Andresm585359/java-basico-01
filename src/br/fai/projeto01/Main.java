8429686efa96c687529529945c37d3251d199b3a
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:06:47 -0300

    primeiro trem

---
+++ src/br/fai/projeto01/Main.java
@@ -0,0 +1,31 @@
+package br.fai.projeto01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+
+	}
+
+	 private void start()
+	 {
+		int idade = 15;
+		System.out.println("A idade é  de  "+idade+ " anos.");
+		idade+=5;
+		idade*=2;
+		System.out.println("resultado1 " +idade);
+		idade-=5;
+		System.out.println("resultado2 "+idade);
+		
+		if (idade>20) {
+			
+			System.out.println("A idade é maior que 20 ");
+		}else {
+			System.out.println("A idade é menor que 20");
+			
+		}
+		
+	 }
+}
